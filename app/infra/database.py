2cb7e4c2d3f84836e4fbf708e51ff4d25c3827ef
Rafael Cascalho <cascalhoeng@gmail.com>
2020-09-01 00:03:05 -0300

    remove unnecessary comments

--- app/infra/database.py
+++ app/infra/database.py
@@ -3,8 +3,6 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from infra import models
 from contextlib import contextmanager
-#from .core.services import EventService
-#from .auth.services import AuthService
 from infra.repositories import UserSQLRepository, TokenRepository
 
 
